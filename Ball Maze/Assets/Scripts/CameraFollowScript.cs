a4f9d43deccc2a291d78d9849dfe9bad51124b66
Paolo Vittorio E. Merle <paolo.merle@hotmail.com>
2016-03-31 10:30:11 +0800

    Le first commit
    all the swags are here

---
+++ Ball Maze/Assets/Scripts/CameraFollowScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowScript : MonoBehaviour {
+
+    private Transform player;
+	// Use this for initialization
+	void Awake () {
+
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        try {
+            transform.position = new Vector3(player.position.x, player.position.y + 15, player.position.z - 5);
+        }
+        catch(MissingReferenceException e)
+        {
+
+        }
+	}
+}
---
+++ Ball Maze/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour{
+
+	public float moveSpeed = 10f;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+
+        rb = GetComponent<Rigidbody>();
+
+	}
+	
+	// Update is called once per frame
+	void Update(){
+	
+	}
+
+	void FixedUpdate () {
+
+		float moveX = Input.GetAxis ("Horizontal");
+		float moveZ = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveX, 0f, moveZ);
+
+        rb.AddForce(movement * moveSpeed);
+
+	}
+
+    void OnCollisionEnter(Collision col) {
+
+        if (col.gameObject.name == "Pits")
+            Destroy(gameObject);
+    }
+}
